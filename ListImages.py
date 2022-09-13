79f441202b317e8034829fb1b548d8ed71486d63
Fabrizio Vitale <59337335+fabriziovit@users.noreply.github.com>
2022-09-13 02:23:32 +0200

    Cambiati i nomi delle immagini.

--- ListImages.py
+++ ListImages.py
@@ -1,5 +1,5 @@
 images = {
     "Tessuto Cuore": ["cuore_tessuto", 0.12500382602],
-    "Immagine 1": ["brightfield", 0.0625],
-    "Test": ["test1", 0.12501949368]
+    "Tessuto polmoni": ["brightfield", 0.0625],
+    "Tessuto": ["test1", 0.12501949368]
 }
